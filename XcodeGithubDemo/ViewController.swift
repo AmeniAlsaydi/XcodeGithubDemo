85ede341724d23d39047483a2d5e590cdb5cc140
Ameni Alsaydi <amenialsaydi@pursuit.org>
2019-10-29 11:30:59 -0400

    second update

--- XcodeGithubDemo/ViewController.swift
+++ XcodeGithubDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .orange
+        print("hey")
     }
 
 
